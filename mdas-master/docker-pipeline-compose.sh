9274b866a88888b05fec6c3a39ece9bebb4bc199
joancliment <45012580+joancliment@users.noreply.github.com>
2018-12-02 21:25:49 +0100

    commit

--- mdas-master/docker-pipeline-compose.sh
+++ mdas-master/docker-pipeline-compose.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e #stops at exception
-image=${REGISTRY}/votingapp:${TAG}
-app="votingapp"
 
 docker-compose down
 docker-compose up -d --build
 docker-compose run --rm votingapp-test
 docker-compose push votingapp
+
 
